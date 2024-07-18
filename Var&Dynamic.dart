3d0c0233f9f78f9160b9b93297857ee56dd7f964
sandeepcs07 <112771475+sandeepcs07@users.noreply.github.com>
2024-07-18 19:14:16 +0000

    Var&dynamic

---
+++ Var&Dynamic.dart
@@ -0,0 +1,24 @@
+void main(){
+
+  print('Var & Dynamic Class');
+
+  String name = "Sandeep";
+  // a="sunny";
+  print(name);
+
+
+  //Var
+  var subject = "CNDC";
+  subject = "UI/UX";
+
+      print(subject);
+
+
+  dynamic rollNo;  //we can use Var instead of dynamic
+  rollNo = 2112127; //int
+  rollNo = "D"; //String
+  rollNo = true;//Bool
+  print(rollNo);
+
+ 
+}
